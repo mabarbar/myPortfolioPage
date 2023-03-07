3db589aa1691149b63ee63d1b56ec54f119de502
mabarbar <mateusz.barzenc00@gmail.com>
2023-03-07 20:21:53 +0100

    trying to solve grid tiles rendering problem

--- src/components/CoolSection.jsx
+++ src/components/CoolSection.jsx
@@ -4,6 +4,8 @@ import Arrow from "./Arrow";
 const CoolSection = ({ executeScroll, goToSectionRef, goToPrevSectionRef }) => {
   let columns = Math.floor(document.body.clientWidth / 50);
   let rows = Math.floor(document.body.clientHeight / 50);
+  let coolArray = [];
+  coolArray.length = columns * rows;
 
   return (
     <section className={styles.coolSection} onClick={() => executeScroll()}>
@@ -13,8 +15,14 @@ const CoolSection = ({ executeScroll, goToSectionRef, goToPrevSectionRef }) => {
         top={"205%"}
         rotate={"180deg"}
       /> */}
-      
-      <div id={styles.tiles}>{columns}</div>
+      <div id={styles.tiles}>
+        {coolArray.map((tile, key) => {
+          <div className="tile" key={key} tile={tile}>
+            s
+          </div>;
+          console.log(1);
+        })}
+      </div>
       {/* <Arrow
         executeScroll={executeScroll}
         goToSectionRef={goToSectionRef}
