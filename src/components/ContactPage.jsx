163b5ea4bebc455bdee3c78f9e93e2099a0a89a9
mabarbar <mateusz.barzenc00@gmail.com>
2023-03-08 00:38:11 +0100

    made responsive grid on click for now

--- src/components/ContactPage.jsx
+++
@@ -1,7 +0,0 @@
-// import styles from "./ContactPage.module.css";
-
-const ContactPage = () => {
-  return <section>ContactPage</section>;
-};
-
-export default ContactPage;
--- src/components/CoolSection.jsx
+++ src/components/CoolSection.jsx
@@ -1,14 +1,28 @@
 import styles from "./CoolSection.module.css";
 import Arrow from "./Arrow";
+import CoolTile from "./CoolTile";
+import Image from "./Image";
+import { useEffect, useState } from "react";
 
 const CoolSection = ({ executeScroll, goToSectionRef, goToPrevSectionRef }) => {
-  let columns = Math.floor(document.body.clientWidth / 50);
-  let rows = Math.floor(document.body.clientHeight / 50);
-  let coolArray = [];
-  coolArray.length = columns * rows;
+  const [columns, setColumns] = useState(
+    Math.floor(document.body.clientWidth / 100)
+  );
+  const [rows, setRows] = useState(
+    Math.floor(document.body.clientHeight / 100)
+  );
+  const [coolArray, setCoolArray] = useState(
+    Array.from({ length: columns * rows }, (v, k) => k)
+  );
+
+  useEffect(() => {
+    setColumns(Math.floor(document.body.clientWidth / 100));
+    setRows(Math.floor(document.body.clientHeight / 100));
+    setCoolArray(Array.from({ length: columns * rows }, (v, k) => k));
+  }, [columns]);
 
   return (
-    <section className={styles.coolSection} onClick={() => executeScroll()}>
+    <section className={styles.coolSection} onClick={setColumns}>
       {/* <Arrow
         executeScroll={executeScroll}
         goToSectionRef={goToPrevSectionRef}
@@ -16,12 +30,9 @@ const CoolSection = ({ executeScroll, goToSectionRef, goToPrevSectionRef }) => {
         rotate={"180deg"}
       /> */}
       <div id={styles.tiles}>
-        {coolArray.map((tile, key) => {
-          <div className="tile" key={key} tile={tile}>
-            s
-          </div>;
-          console.log(1);
-        })}
+        {coolArray.map((item) => (
+          <CoolTile key={item.id} item={item.text} />
+        ))}
       </div>
       {/* <Arrow
         executeScroll={executeScroll}
